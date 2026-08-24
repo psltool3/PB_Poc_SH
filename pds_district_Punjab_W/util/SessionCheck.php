b9ce36dcef063de5d593badc757a003dee97d6cb
Kartikey Katyal <107783109+KARTIKEY-KATYAL@users.noreply.github.com>
2026-08-24 14:47:05 +0530

    incresed session timeout

--- pds_district_Punjab_W/util/SessionCheck.php
+++ pds_district_Punjab_W/util/SessionCheck.php
@@ -17,6 +17,19 @@ else
   }
 
 session_start();
+
+// -----------------------------
+// Session Timeout
+// -----------------------------
+$timeout_duration = 20000;
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout_duration) {
+    session_unset();
+    session_destroy();
+    header("Location: Login.html?error=session_timeout");
+    exit();
+}
+$_SESSION['last_activity'] = time();
+
 if(isset($_SESSION['district_user'])){
 	$user = $_SESSION['district_user'];
 	$token = $_SESSION['district_token'];
